a13e8bd38cad6558508fff0535002fff64383c8c
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 16:48:22 -0400

    feat: mount product router at /api/products

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/api/products", productRoutes);
+
 // Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
