665f2e3ad90ac1f712ea832aaded74622b589bc7
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 15:35:42 -0400

    feat: add mongoose connect with promise handling

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,8 @@
 // Requirements
 const mongoose = require("mongoose");
 
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected Successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
