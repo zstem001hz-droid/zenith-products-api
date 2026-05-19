5c6a622c2755bccf23c2f2d0d1e403c8c59ee642
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 15:57:54 -0400

    feat: add connection event listeners

--- config/connection.js
+++ config/connection.js
@@ -6,3 +6,11 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected Successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+// Connection Events
+const db = mongoose.connection;
+db.on("error", (err) =>
+  console.error("Database Error - check your Atlas connection: " + err.message),
+);
+db.on("connected", () => console.log("MongoDB is connected"));
+db.on("disconnected", () => console.log("MongoDB is disconnected"));
