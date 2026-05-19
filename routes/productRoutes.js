9aa91bd018c4afc22cc9ffbd8112b18d11ef9692
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 17:20:17 -0400

    feat: add POST route to create a product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,4 +3,14 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
+// CREATE - POST /
+router.post("/", async (requestAnimationFrame, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
