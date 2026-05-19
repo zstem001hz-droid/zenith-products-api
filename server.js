2239df073d847493912d30ac62b1b88c728f5e3d
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 16:43:53 -0400

    feat: add express middleware for json and url encoding

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const productRoutes = require("./routes/productRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
