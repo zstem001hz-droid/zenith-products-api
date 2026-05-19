623fa776c30ea34de838a489f186b26d24244894
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 16:18:33 -0400

    chore: add express app configuration

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 require("./config/connection");
 const productRoutes = require("./routes/productRoutes");
+
+// App Configuration
+const app = express();
+const PORT = process.env.PORT || 3001;
