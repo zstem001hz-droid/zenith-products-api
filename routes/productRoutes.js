fe5ce3074bf4a2ab3431709351a0156979ca950a
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-19 20:15:53 -0400

    feat: add GET route to retrieve all products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -54,4 +54,14 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+// INDEX - GET / (Advanced Querying)
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router;
